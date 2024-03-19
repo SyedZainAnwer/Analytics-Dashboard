0029fd15471b433418504b3a0dae44d3c36d5ac5
Syed Zain Anwer <zainanwer24@gmail.com>
2024-03-19 16:37:04 +0100

    fetch nDays

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,5 +1,6 @@
 import { redis } from "@/lib/redis";
 import { getDate } from '@/utils'
+import { parse } from "date-fns";
 
 type AnalyticsArg = {
     retention?: number;
@@ -13,29 +14,46 @@ export class Analytics {
     private retention: number = 60 * 60 * 24 * 7;
 
     constructor(opts?: AnalyticsArg) {
-        if(opts?.retention) this.retention = opts.retention;
+        if (opts?.retention) this.retention = opts.retention;
     }
-    
+
     async track(namespace: string, event: object = {}, opts?: TrackOptions) {
-        
+
         let key = `analytics::${namespace}`
-        
-        if(!opts?.persist) {
+
+        if (!opts?.persist) {
             key += `::${getDate()}`
         }
 
         await redis.hincrby(key, JSON.stringify(event), 1);
 
-        if(!opts?.persist) await redis.expire(key, this.retention);
+        if (!opts?.persist) await redis.expire(key, this.retention);
     }
 
     async retrieveDays(namespace: string, nDays: number) {
-        const promise = [];
+        type AnalyticsPromise = ReturnType<typeof analytics.retrieve>
+        const promises: AnalyticsPromise[] = [];
 
-        for (let index = 0; index < array.length; index++) {
-            const element = array[index];
-            
+        for (let index = 0; index < nDays; index++) {
+            const formattedDate = getDate(index);
+            const promise = analytics.retrieve(namespace, formattedDate);
+            promises.push(promise);
         }
+
+        const fetched = await Promise.all(promises);
+
+        const data = fetched.sort((a, b) => {
+            if (
+                parse(a.date, "dd/MM/yyyy", new Date()) > 
+                parse(b.date, "dd/MM/yyyy", new Date())) 
+            {
+                return 1;
+            } else {
+                return -1;
+            }
+        })
+
+        return data;
     }
 
     async retrieve(namespace: string, date: string) {
