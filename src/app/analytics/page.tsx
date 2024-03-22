9e44633b81452af59f0ae60701b776a3af229d08
Syed Zain Anwer <zainanwer24@gmail.com>
2024-03-22 23:54:12 +0100

    tracking users and displaying it in analytics page

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,13 +1,24 @@
+import AnalyticsDashboard from "@/components/AnalyticsDashboard";
 import { analytics } from "@/utils/analytics";
 
 const Page = async() => {
 
-    const pageview = await analytics.retrieveDays("pageview", 2)
+    const pageviews = await analytics.retrieveDays("pageview", 2);
+    const TRACKING_DAYS = 7;
+    const totalPageviews = pageviews.reduce((acc, curr ) => {
+        return (
+            acc + curr.events.reduce((acc, curr) => {
+                return acc + Object.values(curr)[0]!
+            }, 0)
+        )
+    }, 0);
+
+    const avgVisitorsPerDay = (totalPageviews / TRACKING_DAYS).toFixed(1)
 
     return(
         <div className="min-h-screen w-full py-12 flex justify-center items-center">
             <div className="relative w-full max-w-6xl mx-auto text-white">
-                <AnalyticsDashboard />
+                <AnalyticsDashboard avgVisitorsPerDay="" />
             </div>
         </div>
     )
--- src/components/AnalyticsDashboard.tsx
+++ src/components/AnalyticsDashboard.tsx
@@ -1,8 +1,21 @@
-const AnalyticsDashboard = () => {
+"use client"
+
+import { Card } from "@tremor/react";
+
+interface AnalyticsDashboardProps {
+    avgVisitorsPerDay: string;
+}
+
+const AnalyticsDashboard = ({ avgVisitorsPerDay }: AnalyticsDashboardProps) => {
     return (
         <div className="flex flex-col gap-6">
             <div className="grid w-full mx-auto grid-cols-1 sm:grid-cols-2 gap-6">
-                
+                <Card className="grid w-full mx-auto grid-cols-1 sm:grid-cols-2 gap-6">
+                    <p className="text-tremor-default text-dark-tremor-content">Avg. Visitors/day</p>
+                    <p className="text-3xl text-dark-tremor-content-strong font-semibold">
+                        {avgVisitorsPerDay}
+                    </p>
+                </Card>
             </div>
         </div>
     )
