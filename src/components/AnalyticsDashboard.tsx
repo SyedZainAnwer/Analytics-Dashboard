d409b7e23145c8fc834b41876b68b1d8e42e5846
Syed Zain Anwer <zainanwer24@gmail.com>
2024-03-28 23:55:08 +0100

    badge updated

--- src/components/AnalyticsDashboard.tsx
+++ src/components/AnalyticsDashboard.tsx
@@ -2,6 +2,7 @@
 
 import { analytics } from "@/utils/analytics";
 import { BarChart, Card } from "@tremor/react";
+import { ArrowUpRight } from "lucide-react";
 import ReactCountryFlag from "react-country-flag";
 
 interface AnalyticsDashboardProps {
@@ -11,19 +12,38 @@ interface AnalyticsDashboardProps {
     topCountries: [string, number][];
 }
 
-const Badge = ({ percentage }: {percentage: number}) => {
+const Badge = ({ percentage }: { percentage: number }) => {
     const isPositive = percentage > 0;
     const isNeutral = percentage === 0;
     const isNegative = percentage < 0;
 
-    if(isNaN(percentage)) return null;
+    if (isNaN(percentage)) return null;
+
+    const positiveClassName = 'bg-green-900/25 text-green-400 ring-green-400/25'
+    const neutralClassName = 'bg-zinc-900/25 text-zinc-400 ring-zinc-400/25'
+    const negativeClassName = 'bg-red-900/25 text-red-400 ring-red-400/25'
+
+    return (
+        <span className={`inline-flex gap-1 items-center rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset ${
+            isPositive 
+            ? positiveClassName 
+            : neutralClassName 
+            ? neutralClassName 
+            : negativeClassName}
+        `}>
+            {isPositive ? <ArrowUpRight className="h-3 w-3"/> : null}
+            {isNeutral ? <ArrowUpRight className="h-3 w-3"/> : null}
+            {isNegative ? <ArrowUpRight className="h-3 w-3"/> : null}
+            {percentage.toFixed(0)}%
+        </span>
+    )
 }
 
-const AnalyticsDashboard = ({ 
-    avgVisitorsPerDay, 
-    amtVisitorsToday, 
+const AnalyticsDashboard = ({
+    avgVisitorsPerDay,
+    amtVisitorsToday,
     timeseriesPageviews,
-    topCountries 
+    topCountries
 }: AnalyticsDashboardProps) => {
     return (
         <div className="flex flex-col gap-6">
@@ -35,7 +55,12 @@ const AnalyticsDashboard = ({
                     </p>
                 </Card>
                 <Card className="w-full mx-auto max-w-xs">
-                    <p className="text-tremor-default text-dark-tremor-content">Visitors today</p>
+                    <p className="flex gap-2.5 items-center text-tremor-default text-dark-tremor-content">
+                        Visitors today
+                        <Badge percentage={
+                            (amtVisitorsToday / Number(avgVisitorsPerDay) - 1) * 100
+                        }/>
+                    </p>
                     <p className="text-3xl text-dark-tremor-content-strong font-semibold">
                         {amtVisitorsToday}
                     </p>
@@ -45,15 +70,15 @@ const AnalyticsDashboard = ({
             <Card className="flex flex-col sm:grid grid-cols-4 gap-6">
                 <h2 className="w-full text-dark-tremor-content-strong text-center sm:left-left font-semibold text-xl">This Week's top visitors</h2>
                 <div className="col-span-3 flex items-center justify-between flex-wrap gap-8">
-                    {topCountries?.map(([ countryCode, number ]) =>{
+                    {topCountries?.map(([countryCode, number]) => {
                         return <div className="flex items-center gap-3 text-dark-tremor-strong">
                             <p className="hidden sm:block text-tremor-content">
                                 {countryCode}
                             </p>
-                            <ReactCountryFlag 
-                                className="text-5xl sm:text-3xl" 
-                                svg 
-                                countryCode={countryCode} 
+                            <ReactCountryFlag
+                                className="text-5xl sm:text-3xl"
+                                svg
+                                countryCode={countryCode}
                             />
 
                             <p className="text-tremor-content sm:text-dark-tremor-content-strong">{number}</p>
@@ -73,7 +98,7 @@ const AnalyticsDashboard = ({
                         }))}
                         categories={["Visitors"]}
                         index="name"
-                />
+                    />
                 ) : null}
             </Card>
         </div>
